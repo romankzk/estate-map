86745306d75a2493e2cbe4bbe433336cae19f694
Roman Kozak <romankozak97.ua@gmail.com>
2026-03-23 12:04:52 +0200

    fix: add mobile responsiveness for admin link

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,7 @@ export default function Header() {
                             href="/estate-map/admin"
                             className="flex text-sm font-medium text-gray-900 dark:text-[#F3F4F6] hover:text-gray-700 dark:hover:text-[#F3F4F6] transition-colors"
                         >
-                            <Shield className="w-5 h-5 mr-2" /> Панель адміністратора
+                            <Shield className="w-5 h-5 mr-2" /><span className="hidden lg:inline">Панель адміністратора</span>
                         </Link>
                     {/* )} */}
                     {/* Theme Toggle */}
